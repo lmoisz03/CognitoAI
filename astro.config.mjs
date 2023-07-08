1f700f73d01cfc14961046194ed16e4c8f62ef90
lmoisz <ml776566@gmail.com>
2023-07-08 15:54:00 -0600

    Add vercel analytics and OG image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import preact from '@astrojs/preact'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), preact()]
+	integrations: [tailwind(), preact()],
 	// output: 'server'
+	output: 'server',
+	adapter: vercel({
+		analytics: true
+	})
 })
